f284294a28b3f2752b0273083d29da443e96fd51
StevenDeveloper7 <stevenluck431@gmail.com>
2026-06-10 22:12:59 -0500

    include seeder in run

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\AppointmentType;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,6 +15,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $this->call([
+            RequestTypeSeeder::class,
+            AppointmentTypeSeeder::class,
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
